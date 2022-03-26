afa5a745ecc433f3c73909367bb2e71a2407dd58
alynFF <101980733+alynFF@users.noreply.github.com>
2022-03-26 21:32:10 +0200

    Delete zip/plugin.video.nemesis directory

--- zip/plugin.video.nemesis/resources/contextmenu/play_random.py
+++
@@ -1,5 +0,0 @@
-# -*- coding: utf-8 -*-
-import sys
-from xbmc import executebuiltin
-
-executebuiltin("RunPlugin(%s)" % sys.listitem.getProperty('nemesis_random_params'))
--- zip/plugin.video.nemesis/resources/lib/nemesis.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-	Nemesis Add-on
-"""
-
-from sys import argv
-from modules.router import routing
-routing(argv[2])
